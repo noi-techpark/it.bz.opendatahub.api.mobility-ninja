a9636823e8621078d9727b2d00fda26fc5c35368
Peter Moser <p.moser@noi.bz.it>
2021-09-16 21:57:00 +0200

    Add missing java class

---
+++ src/main/java/it/bz/idm/bdp/ninja/utils/querybuilder/WhereClauseTarget.java
@@ -0,0 +1,53 @@
+package it.bz.idm.bdp.ninja.utils.querybuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.bz.idm.bdp.ninja.utils.miniparser.Token;
+
+public class WhereClauseTarget extends Target {
+
+	private WhereClauseOperator op;
+	private List<Token> values;
+
+	public WhereClauseTarget(String plainText) {
+		super(plainText);
+		//TODO Auto-generated constructor stub
+	}
+
+	public WhereClauseTarget(
+		String alias,
+		WhereClauseOperator whereClauseOperator,
+		Token jsonSel,
+		List<Token> clauseValueTokens)
+	{
+		super(alias, jsonSel == null ? null : jsonSel.getValue(), null);
+		op = whereClauseOperator;
+		if (clauseValueTokens == null) {
+			values = new ArrayList<>();
+		} else {
+			values = clauseValueTokens;
+		}
+	}
+
+	public WhereClauseOperator getOp() {
+		return this.op;
+	}
+
+	public void setOp(WhereClauseOperator op) {
+		this.op = op;
+	}
+
+	public List<Token> getValues() {
+		return this.values;
+	}
+
+	public Token getValue(int index) {
+		return this.values.get(index);
+	}
+
+	public void addValue(Token value) {
+		this.values.add(value);
+	}
+
+}
