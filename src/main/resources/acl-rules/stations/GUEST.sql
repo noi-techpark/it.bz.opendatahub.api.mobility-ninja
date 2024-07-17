384847fd579d16c4ac39e7c3480c6d580ff4a3c2
Clemens Zagler <c.zagler@noi.bz.it>
2024-07-17 09:49:27 +0200

    added parkingstations origin=sta to open data

--- src/main/resources/acl-rules/stations/GUEST.sql
+++ src/main/resources/acl-rules/stations/GUEST.sql
@@ -47,7 +47,7 @@
 	or (s.stationtype = 'EnvironmentStation' and s.origin = 'APPATN-open')
 	or (s.stationtype = 'LinkStation' and (s.origin is null or s.origin = 'NOI'))
 	or (s.stationtype = 'MeteoStation' and s.origin in ('meteotrentino', 'SIAG', 'EURAC'))
-	or (s.stationtype = 'ParkingStation' and s.origin in ('FAMAS', 'FBK', 'Municipality Merano', 'skidata', 'Municipality Bolzano'))
+	or (s.stationtype = 'ParkingStation' and s.origin in ('FAMAS', 'FBK', 'Municipality Merano', 'skidata', 'Municipality Bolzano', 'STA'))
 	or (s.stationtype = 'RWISstation' and s.origin = 'InfoMobility')
 
     -- special rules
